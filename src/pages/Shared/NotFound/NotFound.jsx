c4a9175d2db004bcacbd1b3600505277b773426d
dev-nazmulislam <developer.nazmulislam@gmail.com>
2022-04-18 20:05:11 +0600

    revision final

--- src/pages/Shared/NotFound/NotFound.jsx
+++ src/pages/Shared/NotFound/NotFound.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center py-24">
+    <div className="d-flex justify-content-center align-items-center py-5">
       <div>
-        <h1 className="text-9xl">404</h1>
+        <h1 className="fs-1">404</h1>
       </div>
       <div>
-        <h2 className="font-medium">Oops! You're lost.</h2>
+        <h2 className="fs-4">Oops! You're lost.</h2>
         <p>The page you are looking for was not found.</p>
       </div>
     </div>
